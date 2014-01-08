d2c804d8db9cf704fe654508faa7790768dab66b
kwynns <kelseywynns@gmail.com>
2014-01-08 15:54:59 -0600

    Added vu brand bar

---
+++ templates/vubrandbar.php
@@ -0,0 +1,50 @@
+<link rel="stylesheet" href="http://www.vanderbilt.edu/asset/css/brandbar.css?v=201304" type="text/css" media="screen, projection" />
+<script type="text/javascript" src="http://www.vanderbilt.edu/asset/scripts/brandbar.js" ></script>
+<!--[if IE 6]><div id="ie6"><![endif]-->
+<div id="vunavigate"  style="display:none;">
+<ul>
+<li><a href="http://www.vanderbilt.edu/about/">About</a></li>
+<li><a href="http://www.vanderbilt.edu/prospective/">Admissions</a></li>   
+<li><a href="http://www.vanderbilt.edu/academics/">Academics</a></li>  
+<li><a href="http://research.vanderbilt.edu/">Research</a></li>  
+<li><a href="http://www.vanderbilt.edu/student/">Students</a></li>
+<li><a href="http://www.vanderbilt.edu/faculty-staff/">Faculty &amp; Staff</a></li>  
+<li><a href="http://www.vucommodores.com/">Athletics</a></li>
+<li class="last"><a href="http://www.mc.vanderbilt.edu/">Medical Center</a></li>  
+</ul></div>
+<div id="vutools"  style="display:none;">
+<ul>
+<li><a href="http://www.vanderbilt.edu/email/">Email</a></li>  
+<li><a href="http://yes.vanderbilt.edu/">YES</a></li>
+<li><a href="http://www.vanderbilt.edu/oak/">OAK</a></li>   
+<li><a href="https://webapp.mis.vanderbilt.edu/c2hr/">c2hr</a></li>
+<li><a href="http://www.vanderbilt.edu/logins/">Logins</a></li>
+<li><a href="http://library.vanderbilt.edu/">Libraries</a></li>
+<li><a href="http://calendar.vanderbilt.edu/">Calendar</a></li>
+<li><a href="https://phonedirectory.vanderbilt.edu/cdb/index.jsp">People Finder</a></li>        
+<li><a href="http://www.vanderbilt.edu/map">Campus Maps</a></li>
+<li class="last"><a href="http://www.vanderbilt.edu/atoz/letter/A">A-Z</a></li>  
+</ul></div>
+<div id="vusearch"  style="display:none;">
+<div id="searchme"><form method="get" action="http://searchvu.vanderbilt.edu/search">
+<input class="searchbox" name="q" maxlength="256" value="SEARCH" type="text" onfocus="clearDefault(this)" />
+<input type="hidden" name="site" value="default_collection" />
+<input type="hidden" name="client" value="default_frontend" />
+<input type="hidden" name="proxystylesheet" value="default_frontend" />
+<input type="hidden" name="output" value="xml_no_dtd" />
+<input type="hidden" name="getfields" value="*" />
+<input type="hidden" name="numgm" value="15" />
+<input class="searchbtn" name="go" tabindex="2" type="submit" value="GO" />	
+</form></div></div>
+<div id="vubrandbar">
+<div class="vuinsider">
+<div class="vulogomini"><h1><a href="http://www.vanderbilt.edu"><img src="http://www.vanderbilt.edu/asset/i/vanderbilt-print.jpg" alt="Vanderbilt University"/></a></h1></div>
+<div id="toolsvu">
+<ul>
+<li><a href="http://www.vanderbilt.edu">Vanderbilt Home</a></li>
+<li><a class="vunavigate" href="#">Navigate VU</a></li>
+<li><a class="vutools" href="#">Tools</a></li>
+<li><a class="vusearch last" href="#">Search VU</a></li>
+</ul></div></div></div>
+<!--[if IE 6]></div><![endif]-->
+<script type="text/javascript" src="http://www.vanderbilt.edu/asset/emergency.js" ></script>
