c1360bf9368a044e6d9eac49447c100f2e332721
Kelsey Wynns <kelseywynns@gmail.com>
2014-01-10 15:19:15 -0600

    Removed lib folder

--- functions.php
+++ functions.php
@@ -3,12 +3,21 @@
  * Custom functions
  */
  
-//define('REDUX_OPT_NAME', 'shoestrap');
+ 
+//load custom modules
 require_once locate_template('/modules/banner/module.php');
-//require_once locate_template('/modules/footer/module.php');
+
+//load over-rides
+require_once locate_template('/overrides/footer.php');
 
 
+//adds style.css to head
 function addStyles() {
     wp_enqueue_style('shoestrap-vu-css', get_stylesheet_directory_uri().'/style.css');
 }
 add_action('wp_head', 'addStyles');
+
+//include widgets
+require_once locate_template('/widgets/widgets.php');
+
+?>
