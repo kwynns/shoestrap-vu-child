506ac50cd26c36d815397e1faa75966aef63359c
Kelsey Wynns <kelseywynns@gmail.com>
2014-01-10 17:15:42 -0600

    updated based on shoestrap child theme example

--- functions.php
+++ functions.php
@@ -2,7 +2,19 @@
 /**
  * Custom functions
  */
- 
+// Prioritize loading of some necessary core modules
+if ( file_exists( get_template_directory() . '/lib/modules/core.redux/module.php' ) ) :
+        require_once get_template_directory() . '/lib/modules/core.redux/module.php';
+endif;
+if ( file_exists( get_template_directory() . '/lib/modules/core/module.php' ) ) :
+        require_once get_template_directory() . '/lib/modules/core/module.php';
+endif;
+if ( file_exists( get_template_directory() . '/lib/modules/core.layout/module.php' ) ) :
+        require_once get_template_directory() . '/lib/modules/core.layout/module.php';
+endif;
+if ( file_exists( get_template_directory() . '/lib/modules/core.images/module.php' ) ) :
+        require_once get_template_directory() . '/lib/modules/core.images/module.php';
+endif;
  
 //load custom modules
 require_once locate_template('/modules/banner/module.php');
