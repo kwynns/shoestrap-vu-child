969e2d2802aa8b9e0cf59ac8c91b7e34a679262d
Kelsey Wynns <kelseywynns@gmail.com>
2014-01-10 15:21:35 -0600

    Added social widget (incomplete)
    Hoping I’ll be able to pull in the social links from the theme options
    into this widget

---
+++ widgets/widgets.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Custom functions
+ */
+ add_action( 'shoestrap_social_widget', 'shoestrap_social_sharing', 5 );
+
+class Show_social extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'social_widget', // Base ID
+			__('Shoe Social Icons', 'text_domain'), // Name
+			array( 'description' => __( 'A widget to show social icons', 'text_domain' ), ) // Args
+		);
+	}
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+			echo $args['before_title'] . $title . $args['after_title'];
+		//echo __( shoestrap_social_sharing(), 'text_domain' );
+		echo "social here";
+		do_action( 'shoestrap_social_widget' );
+		echo $args['after_widget'];
+	}
+/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( 'New title', 'text_domain' );
+		}
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<?php 
+	}
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+	
+
+}
+
+add_action( 'widgets_init', function(){
+     register_widget( 'Show_social' );
+});
+	//shoestrap_social_sharing();
