7194cfbeca05368c9efc4e73e376d6c9af3b6a29
Kelsey Wynns <kelseywynns@gmail.com>
2014-01-10 12:12:50 -0600

    Enabled banner in child theme

--- functions.php
+++ functions.php
@@ -2,6 +2,11 @@
 /**
  * Custom functions
  */
+ 
+//define('REDUX_OPT_NAME', 'shoestrap');
+require_once locate_template('/modules/banner/module.php');
+//require_once locate_template('/modules/footer/module.php');
+
 
 function addStyles() {
     wp_enqueue_style('shoestrap-vu-css', get_stylesheet_directory_uri().'/style.css');
